d85eed7a28ab545adfd2512154b36e81f4b4d209
hiMinju <minju980508@gmail.com>
2020-09-21 20:40:19 +0900

    Rewrite the code as simple

--- 2020-09/RobotCleaner.cpp
+++ 2020-09/RobotCleaner.cpp
@@ -1,4 +1,4 @@
-// 2020-09-20
+// 2020-09-20 ~ 2020-09-21
 // Minju Kim
 // Baekjoon no.14503
 // using simulation
@@ -6,47 +6,42 @@
 #include <iostream>
 using namespace std;
 
-int room[50][50];
-int num = 0, dir = 0, r, c, result = 0;
-bool flag = false;
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	int map[50][50];
+	int N, M, x, y, dir;
 
-	int N, M, x, y;
 	cin >> N >> M;
 	cin >> x >> y >> dir;
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			int temp;
-			cin >> temp;
-			room[i][j] = temp;
-			if (temp == 0) ++num;
+			cin >> map[i][j];
 		}
 	}
 
 	int dx[4] = { -1, 0, 1, 0 };
-	int dy[4] = { 0, 1, 0 ,-1 };
+	int dy[4] = { 0, 1, 0, -1 };
+	int result = 0;
 
 	while (true) {
-		if (room[x][y] == 0) { // 1번 현재 위치 청소
-			room[x][y] = 2;
+		if (map[x][y] == 0) { // 현재 부분 청소
+			map[x][y] = 2;
 			++result;
 		}
 
 		int check = 0;
-		for (int i = 0; i < 4; i++) { // 네 방향 모두 탐색
+		for (int i = 0; i < 4; i++) {
 			dir = (dir + 3) % 4;
-
 			int tempX = x + dx[dir];
 			int tempY = y + dy[dir];
-			if (room[tempX][tempY] == 0) { // 청소할 곳을 찾으면
+
+			if (map[tempX][tempY] == 0) { // a조건 만족
+				check = 1;
 				x = tempX;
 				y = tempY;
-				check = 1;
 				break;
 			}
 		}
@@ -54,14 +49,13 @@ int main() {
 			continue;
 
 		// 후진
-		int temp_dir = dir - 2;
 		x += dx[(dir + 2) % 4];
 		y += dy[(dir + 2) % 4];
-
-		if (room[x][y] == 1)
+		if (map[x][y] == 1) { // 종료 조건
 			break;
+		}
 	}
-
 	cout << result;
+
 	return 0;
 }
